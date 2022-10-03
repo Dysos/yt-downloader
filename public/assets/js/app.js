1fd5857a454fe32ead52687d05bef3a0bd84829d
Dysos <victorj94@aol.com>
2022-10-03 22:46:31 +0200

    changed class names to BEM

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,6 @@
-const dlButton = document.querySelector('.download__button');
-const textInput = document.querySelector('.text__input');
-dlButton.addEventListener('click', () => {
+const downloadButton = document.querySelector('.download-button');
+const textInput = document.querySelector('.text-input');
+downloadButton.addEventListener('click', () => {
 	const encoded = encodeURIComponent(textInput.value);
-
 	location.href = `/download/${encoded}`;
 });
